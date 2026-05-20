01063b48edfae3151b2509fa42b1f1c298b07110
TheTimesweeper <leo.midget@gmail.com>
2026-05-20 10:32:18 -0700

    woops forgote

--- Assets/_BobotGame/Scripts/Spellcasting/SpellCasting/ActiveStates/DedState.cs
+++ Assets/_BobotGame/Scripts/Spellcasting/SpellCasting/ActiveStates/DedState.cs
@@ -1,7 +1,14 @@
 ﻿namespace ActiveStates
 {
-    public class DedState : ActiveState
+    public class DedState : BasicTimedStateSimple
     {
+        protected override float baseDuration => 0;
 
+        public override void OnExit(bool machineDed = false)
+        {
+            base.OnExit(machineDed);
+
+            UnityEngine.Object.Destroy(gameObject);
+        }
     }
 }
